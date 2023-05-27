32ec4ae89740db8c843408b2d8c0586dd4245241
Aarow Epstein <aarowepstein1@gmail.com>
2023-05-26 20:22:11 -0700

    commit tree 75438be08032a9860f3e91bdaa938354d5c14fbb

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,5 +1,5 @@
 require 'pry'
-
+ 
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
 	puts inside_the_method
@@ -8,5 +8,5 @@ def prying_into_the_method
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
 	puts this_variable_hasnt_been_interpreted_yet
 end
-
+ 
 prying_into_the_method
